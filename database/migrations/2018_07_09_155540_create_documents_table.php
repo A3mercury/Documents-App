44302f8032b59e9283e3c0a5c6dc7a7a02943706
Austin Andrews <a5mercuryhu@gmail.com>
2018-07-09 16:36:37 -0500

    Create new document migraiton and remove users

---
+++ database/migrations/2018_07_09_155540_create_documents_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDocumentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('documents', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->default('');
+            $table->string('description')->default('');
+            $table->string('file_path')->default('');
+            $table->string('file_name')->default('');
+            $table->string('file_type')->default('');
+            $table->string('file_size')->default('');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('documents');
+    }
+}
